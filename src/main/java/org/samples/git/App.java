ae28707a94bd87acbd3920b3fb3ce1d7e7dca725
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2019-04-23 16:06:51 +0300

    1st commit user-story4

--- src/main/java/org/samples/git/App.java
+++ src/main/java/org/samples/git/App.java
@@ -10,5 +10,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("User Story One");
         System.out.println( "1st commit User Story 2" );
+        System.out.println( "1st commit User Story4" );
     }
 }

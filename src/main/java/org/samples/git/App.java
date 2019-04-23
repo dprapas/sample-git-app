796b6baba60e7e4de531f3cf2fb36a719bb32314
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2019-04-23 20:46:58 +0300

    Reinitialize

--- src/main/java/org/samples/git/App.java
+++ src/main/java/org/samples/git/App.java
@@ -7,13 +7,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("User Story One");
-        System.out.println( "1st commit User Story 2" );
-        System.out.println( "1st commit User Story4" );
-        System.out.println( "2nd commit User Story4" );
-        System.out.println( "3rd commit User Story4" );
-        System.out.println( "1st commit User Story 3" );
-        System.out.println( "2nd commit User Story 3" );
+        
     }
 }

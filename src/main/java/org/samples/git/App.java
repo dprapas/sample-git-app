08d5f99c370a82fec57df7d542767d64f2da32f5
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2019-04-23 20:49:45 +0300

    2nd commit user-story5

--- src/main/java/org/samples/git/App.java
+++ src/main/java/org/samples/git/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("1st commit user-story5");
+        System.out.println("2nd commit user-story5");
     }
 }

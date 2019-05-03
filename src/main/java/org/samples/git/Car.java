618525cf07cfe119ac11c9acd11694f4a6eb78fc
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2019-05-03 11:32:46 +0300

    1st commit user-story-1

--- src/main/java/org/samples/git/Car.java
+++ src/main/java/org/samples/git/Car.java
@@ -4,6 +4,13 @@ public class Car {
 
     private String name;
 
+    private String brand;
+
+    public Car(String name, String brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
     public Car(String name) {
         this.name = name;
     }
@@ -14,5 +21,13 @@ public class Car {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 }

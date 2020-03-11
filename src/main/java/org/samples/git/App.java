b0acb0bc4b9321b243eeeac366f1b59318d65f87
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2020-03-11 11:35:37 +0200

    user-story-1.1

--- src/main/java/org/samples/git/App.java
+++ src/main/java/org/samples/git/App.java
@@ -6,6 +6,7 @@ package org.samples.git;
 public class App {
 
     public static void main(String[] args) {
+        System.out.println("user-story-1.1");
         System.out.println("user-story-2.1");
     }
 }

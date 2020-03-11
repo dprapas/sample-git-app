9012abe0d0740343ac5f390b856ff9aaad77c366
Dimitrios Prapas <dimitrios.prapas@prodyna.com>
2020-03-11 11:30:03 +0200

    Back to initial state

--- src/main/java/org/samples/git/App.java
+++ src/main/java/org/samples/git/App.java
@@ -6,9 +6,5 @@ package org.samples.git;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("1st commit user-story-2");
-        System.out.println("2nd commit user-story-2");
-        System.out.println("2nd commit user-story-1");
-        System.out.println("3rd commit user-story-2");
     }
 }
